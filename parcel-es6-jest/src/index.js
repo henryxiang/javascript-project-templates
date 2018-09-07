bfd11d54685d89023bdd83543499098c6ca95798
Henry Xiang <hxxiang@ucdavis.edu>
2018-09-06 17:51:52 -0700

    added SVG demo

--- parcel-es6-jest/src/index.js
+++ parcel-es6-jest/src/index.js
@@ -4,6 +4,14 @@ import Snap from 'snapsvg-cjs';
 const app = document.querySelector('#app');
 app.innerHTML = greet('World');
 
-const snap = Snap('#svg');
-const circle = snap.circle(100, 100, 50).attr({ fill: 'blue' });
-circle.animate({ transform: 't500, 500'}, 3000, mina.bounce);
+const snap = Snap('#svg').attr({ width: 500, height: 500 });
+const circle1 = snap.circle(100, 100, 50).attr({ fill: 'blue' });
+const circle2 = snap.circle(400, 400, 50).attr({ fill: 'red' });
+circle1.animate(
+  { transform: 't300,300'},
+  3000, mina.bounce
+);
+circle2.animate(
+  { transform: 't-300,-300'},
+  3000, mina.bounce
+);

fe1d6d92d4a35cac4758bbc9a1a411a6469b89bb
Henry Xiang <hxxiang@ucdavis.edu>
2018-09-06 17:37:11 -0700

    added SVG feature to parcel-es6-jest template

--- parcel-es6-jest/src/index.js
+++ parcel-es6-jest/src/index.js
@@ -1,4 +1,9 @@
 import greet from './dummy';
+import Snap from 'snapsvg-cjs';
 
 const app = document.querySelector('#app');
 app.innerHTML = greet('World');
+
+const snap = Snap('#svg');
+const circle = snap.circle(100, 100, 50).attr({ fill: 'blue' });
+circle.animate({ transform: 't500, 500'}, 3000, mina.bounce);
